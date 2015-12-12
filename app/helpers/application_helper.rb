cea35bcb6b6241153a27efa3b7cbdbc50d01462a
Raquel Moss <raquelxmoss@gmail.com>
2015-12-12 21:40:54 +1300

    Errant comma

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,6 @@ end
 
 def create_initial_user_article_states(current_user, feed)
   feed.articles.each do |article|
-    current_user.user_article_states.create(article_id: article.id, )
+    current_user.user_article_states.create(article_id: article.id)
   end
 end
